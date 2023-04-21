b505944185e3454ef0d6bdf5e60ebab5d88bd9de
Abdul Majid <76196721+majid-2002@users.noreply.github.com>
2023-04-21 18:25:18 +0530

    :lipstick: update

--- src/components/MemeGenerator.jsx
+++ src/components/MemeGenerator.jsx
@@ -2,6 +2,29 @@ import React from "react";
 import "./style.css";
 
 function MemeGenerator() {
+  const [meme, setMeme] = React.useState({
+    top: "",
+    bottom: "",
+    randomImg: "https://i.imgflip.com/1bij.jpg",
+  });
+
+  function getnewMeme(e) {
+    e.preventDefault();
+    fetch("https://api.imgflip.com/get_memes")
+      .then((response) => response.json())
+      .then((response) => {
+        const { memes } = response.data;
+        const randomNum = Math.floor(Math.random() * memes.length);
+        const randomMemeImg = memes[randomNum].url;
+        setMeme({ ...meme, randomImg: randomMemeImg });
+      });
+  }
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setMeme({ ...meme, [name]: value });
+  }
+
   return (
     <div>
       <header className="header-meme">
@@ -15,10 +38,29 @@ function MemeGenerator() {
       <main className="main-meme">
         <form className="meme-form">
           <div className="row">
-            <input type="text" name="topText" placeholder="Top Text" />
-            <input type="text" name="bottomText" placeholder="Bottom Text" />
+            <input
+              type="text"
+              name="top"
+              placeholder="Top Text"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              name="bottom"
+              placeholder="Bottom Text"
+              onChange={handleChange}
+            />
           </div>
-          <button>Get a new Image</button>
+          <button onClick={getnewMeme}>Get a new Image</button>
+          <div className="meme">
+            <img src={meme.randomImg} alt="Meme" className="meme-img" />
+            <h2 className="top-text meme-text">
+              {meme.top}
+            </h2>
+            <h2 className="bottom-text meme-text">
+              {meme.bottom}
+            </h2>
+          </div>
         </form>
       </main>
     </div>
